344ccba17dda1ee870eeb6f96bde705f8c76f822
JayB <ajiboyeabiodun001@gmail.com>
2022-03-26 12:06:41 +0100

    new commit

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const OpenMenu = document.getElementById('open_menu')
       CloseMenu = document.getElementById('close_menu')
       Nav=document.querySelector('nav');
+      Menu=document.getElementById('menu1')
 
       OpenMenu.addEventListener('click', () => {
-          Nav.classlist.add('mobile_menu');
+          //Nav.classlist.add('mobile_menu');
+          Nav.style.display="block";
       })
 
       CloseMenu.addEventListener('click', () => {
-        Nav.classlist.remove('mobile_menu');
+        Nav.style.display ="none";
+    })
+
+    Menu.addEventListener('click', () => {
+        Nav.style.display = "none";
     })
